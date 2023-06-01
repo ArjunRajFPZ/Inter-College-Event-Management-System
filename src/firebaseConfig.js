76b353a5d4814770e863bb910eea9810369acb10
ARJUN RAJ R S <95402319+ArjunRajFPZ@users.noreply.github.com>
2023-06-01 21:01:33 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,7 @@ import { getStorage } from "firebase/storage";
  
 // Initialize Firebase
 const app = initializeApp ({
-    apiKey: "AIzaSyDgpKnIYRAeQoLpIOTuwiofXHR9Q4d82sA",
-    authDomain: "eventmanagement-c9302.firebaseapp.com",
-    projectId: "eventmanagement-c9302",
-    storageBucket: "eventmanagement-c9302.appspot.com",
-    messagingSenderId: "209929917221",
-    appId: "1:209929917221:web:67ecf7609ef55da73940cc",
-    measurementId: "G-3NVCW3PSS2"
+//     PUT FIREBASE CONFIGRATION HERE
 });
 
 // Firebase storage reference
