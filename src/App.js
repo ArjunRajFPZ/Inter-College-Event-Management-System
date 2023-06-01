3d297afe319b765b5686045bcecd9e45c688b31c
ARJUN RAJ R S <95402319+ArjunRajFPZ@users.noreply.github.com>
2023-06-01 20:57:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,13 +24,7 @@ const Home = React.lazy(() => import('./views/pages/home/Home'))
 const Userid = sessionStorage.getItem("id");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDgpKnIYRAeQoLpIOTuwiofXHR9Q4d82sA",
-  authDomain: "eventmanagement-c9302.firebaseapp.com",
-  projectId: "eventmanagement-c9302",
-  storageBucket: "eventmanagement-c9302.appspot.com",
-  messagingSenderId: "209929917221",
-  appId: "1:209929917221:web:67ecf7609ef55da73940cc",
-  measurementId: "G-3NVCW3PSS2"
+//   PUT YOUR FIREBASE CONFIGRATION HERE
 };
 
 const app = initializeApp(firebaseConfig);
